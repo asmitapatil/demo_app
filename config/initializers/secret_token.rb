dcc6a1f9effe7129bd650aeba9d31f28d98a58ca
asmi <apatil@entrib.com>
2013-06-16 22:49:17 +0530

    Initial commit of demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f84bd3c5e56c1aea50cf9086324113e36fcf3b173f5111fe513c8617565b10588b5c0ad4998a9395d30fe6f33665c0de8b5550c9f50485728eda229e55886df9'
